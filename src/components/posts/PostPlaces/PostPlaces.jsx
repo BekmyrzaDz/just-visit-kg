bd06efc800257eb213b64bff0e82e6989b0712ac
Bekmyrza <msn.10110922@gmail.com>
2022-11-17 00:54:57 +0600

    Completed the functionality of the hotels subsection in the places section

--- src/components/posts/PostPlaces/PostPlaces.jsx
+++ src/components/posts/PostPlaces/PostPlaces.jsx
@@ -30,6 +30,19 @@ import interestPlacesCarousel7 from "../../../assets/images/interest-places7.jpg
 import interestPlacesCarousel8 from "../../../assets/images/interest-places8.jpg";
 import interestPlacesCarousel9 from "../../../assets/images/interest-places9.jpg";
 import interestPlacesCarousel10 from "../../../assets/images/interest-places10.jpg";
+// interest places images
+import hotel1 from "../../../assets/images/hotel1.jpg";
+import hotel2 from "../../../assets/images/hotel2.jpg";
+import hotel3 from "../../../assets/images/hotel3.jpg";
+import hotel4 from "../../../assets/images/hotel4.jpg";
+import hotel5 from "../../../assets/images/hotel5.jpg";
+import hotel6 from "../../../assets/images/hotel6.jpg";
+import hotel7 from "../../../assets/images/hotel7.jpg";
+import hotel8 from "../../../assets/images/hotel8.jpg";
+import hotel9 from "../../../assets/images/hotel9.jpg";
+import hotel10 from "../../../assets/images/hotel10.jpg";
+import hotel11 from "../../../assets/images/hotel11.jpg";
+import hotel12 from "../../../assets/images/hotel12.jpg";
 
 const data = {
   toursData: {
@@ -146,7 +159,30 @@ const data = {
       },
     ],
   },
-  hotelsData: [],
+  hotelsData: [
+    { title: "Orion Hotel", img: hotel1, rating: "5,0", location: "Бишкек" },
+    { title: "Sheraton", img: hotel2, rating: "4,5", location: "Бишкек" },
+    { title: "Novotel", img: hotel3, rating: "5,0", location: "Бишкек" },
+    { title: "Jannat Regency", img: hotel4, rating: "4,5", location: "Бишкек" },
+    { title: "Ambassador", img: hotel5, rating: "4,5", location: "Бишкек" },
+    {
+      title: "Ramada by Wyndham",
+      img: hotel6,
+      rating: "4,5",
+      location: "Бишкек",
+    },
+    { title: "City Hotel", img: hotel7, rating: "4,5", location: "Бишкек" },
+    { title: "Onyx Hotel", img: hotel8, rating: "4,0", location: "Бишкек" },
+    { title: "Soluxe Hotel", img: hotel9, rating: "4,5", location: "Бишкек" },
+    { title: "Madison Ave", img: hotel10, rating: "4,5", location: "Бишкек" },
+    {
+      title: "Silk Road lodge",
+      img: hotel11,
+      rating: "4,0",
+      location: "Бишкек",
+    },
+    { title: "Grand Hotel", img: hotel12, rating: "4,5", location: "Бишкек" },
+  ],
   resataurantsData: [],
 };
 
